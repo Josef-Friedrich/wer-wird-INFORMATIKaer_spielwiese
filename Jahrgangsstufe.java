204dc86680efc568e9735760a5b1d08a87079493
Josef Friedrich <josef@friedrich.rocks>
2020-07-03 22:33:10 +0200

    Hinzufügen von Kommentaren

--- Jahrgangsstufe.java
+++ Jahrgangsstufe.java
@@ -1,17 +1,40 @@
 import java.util.Random;
 
+/**
+ * Eine Jahrgangstufe beinhaltet eine gewissen Anzahl an Fragen.
+ *
+ * Sie ist als eine einfach verkette Liste nach dem Entwurfsmuster
+ * „Kompositum“ realisiert.
+ *
+ * Sie entspricht der Klasse „Liste“ aus dem AUD 4 Beispiel von Annabel.
+ *
+ * @see https://www.studon.fau.de/file2861024_download.html
+ */
 public class Jahrgangsstufe {
   private ListenElement kopf;
+
+  /**
+   * Die Anzahl an Fragen kann zwar rekursiv durch die Methode
+   * {@link gibAnzahlFragen} bestimmt werden. Das ist aber etwas
+   * zeitintensiv bei vielen Fragen.
+   */
+  private int anzahlFragen = 0;
 
   public Jahrgangsstufe() {
     kopf = new ListenAbschluss();
   }
 
+  public void fügeVorneEin(Frage frage) {
+    DatenKnoten neuerKnoten = new DatenKnoten(kopf, frage);
+    kopf = neuerKnoten;
+  }
+
+
   public void fügeHintenEin(Frage frage) {
     kopf = kopf.fügeHintenEin(frage);
   }
 
-  public void fügeVorneEin(Frage frage) {
+  public void fügeNachPositionEin(Frage frage, int position) {
     DatenKnoten neuerKnoten = new DatenKnoten(kopf, frage);
     kopf = neuerKnoten;
   }
@@ -34,5 +57,4 @@ public class Jahrgangsstufe {
     kopf = kopf.gibNächstes();
     return frage;
   }
-
 }
--- Text.java
+++ Text.java
@@ -2,26 +2,17 @@ import ch.aplu.jgamegrid.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
-
 public class Text extends TextActor implements GGKeyListener {
 
-  public Text (String text) {
-    super(
-      false,
-      text,
-      java.awt.Color.WHITE,
-      java.awt.Color.BLACK,
-      new Font("Sans", Font.BOLD, 20)
-    );
+  public Text(String text) {
+    super(false, text, java.awt.Color.WHITE, java.awt.Color.BLACK, new Font("Sans", Font.BOLD, 20));
   }
 
-  public boolean keyPressed(KeyEvent evt)
-  {
-    return false;  // Don't consume the key
+  public boolean keyPressed(KeyEvent evt) {
+    return false; // Don't consume the key
   }
 
-  public boolean keyReleased(KeyEvent evt)
-  {
+  public boolean keyReleased(KeyEvent evt) {
     return false;
   }
 
