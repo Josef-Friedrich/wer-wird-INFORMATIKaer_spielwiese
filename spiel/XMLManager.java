997c2644e6f11b264f3a4b2035892420a8244b69
Josef Friedrich <josef@friedrich.rocks>
2020-08-13 17:01:43 +0200

    Die Klassen XMLLeser und XMLSchreiber in eine Klasse vereinigen

--- spiel/XMLManager.java
+++ spiel/XMLManager.java
@@ -1,6 +1,8 @@
 package spiel;
 
 import java.io.File;
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,10 +12,16 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-public class XMLSchreiber {
+public class XMLManager {
 
   Element wurzel;
   Document dokument;
@@ -24,7 +32,18 @@ public class XMLSchreiber {
   int fragenZähler = 0;
   Element fach;
 
-  public XMLSchreiber() {
+  public XMLManager(File datei)  {
+    try {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      dokument = db.parse(datei);
+      dokument.getDocumentElement().normalize();
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public XMLManager() {
     try {
       DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
       DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
@@ -51,6 +70,25 @@ public class XMLSchreiber {
     }
   }
 
+  /**
+   * Lese die Metadaten des Fragen-Katalogs. Metadaten sind direkt
+   * unter dem Wurzel-Element fragenKatalog angehängt und haben nur
+   * einen Text-Inhalt.
+   *
+   * @param elementName Der Name des XML-Elements z. b. thema, autor
+   *
+   * @return Der Textinhalt des Elements.
+   */
+  public String leseMetaDaten(String elementName) {
+    NodeList nodeList = dokument.getElementsByTagName(elementName);
+    Node node = nodeList.item(0);
+    if (node == null) {
+      return "";
+    } else {
+      return node.getTextContent();
+    }
+  }
+
   private void erzeugeText(Element element, String text) {
     element.appendChild(dokument.createTextNode(text));
   }
@@ -65,12 +103,28 @@ public class XMLSchreiber {
     element.appendChild(erzeugeElementMitText(elementName, text));
   }
 
-  public void setzeAutor(String autor) {
-    erzeugeText(this.autor, autor);
+  public void setzeFach(String fach) {
+    erzeugeText(this.fach, fach);
+  }
+
+  public String gibFach() {
+    return leseMetaDaten("fach");
   }
 
   public void setzeThema(String thema) {
     erzeugeText(this.thema, thema);
+  }
+
+  public String gibThema() {
+    return leseMetaDaten("thema");
+  }
+
+  public void setzeAutor(String autor) {
+    erzeugeText(this.autor, autor);
+  }
+
+  public String gibAutor() {
+    return leseMetaDaten("autor");
   }
 
   public void setzeAnzahFragen(String anzahlFragen) {
@@ -79,10 +133,6 @@ public class XMLSchreiber {
 
   public void setzeAnzahFragen() {
     erzeugeText(this.anzahlFragen, String.valueOf(fragenZähler));
-  }
-
-  public void setzeFach(String fach) {
-    erzeugeText(this.fach, fach);
   }
 
   public Element setzeFrage(String fragenText, String richtigeAntwort, String falscheAntwort1, String falscheAntwort2,
@@ -117,8 +167,34 @@ public class XMLSchreiber {
       DOMSource domSource = new DOMSource(dokument);
       StreamResult streamResult = new StreamResult(new File(dateiName));
       transformer.transform(domSource, streamResult);
-    } catch ( TransformerException e) {
+    } catch (TransformerException e) {
       e.printStackTrace();
     }
   }
+
+  public void konvertiereCSV(String pfad) {
+    try {
+      CSVLeser csvLeser = new CSVLeser(pfad);
+      CSVParser csv = csvLeser.gibLeser();
+      setzeFach("Informatik");
+      setzeThema("Jahrgangsstufe");
+      setzeAutor("Michi, Steffi, Josef, Martin");
+
+      for (CSVRecord csvRecord : csv) {
+        setzeFrage(csvRecord.get("fragenText"), csvRecord.get("richtigeAntwort"), csvRecord.get("falscheAntwort1"),
+            csvRecord.get("falscheAntwort2"), csvRecord.get("falscheAntwort3"),
+            Integer.parseInt(csvRecord.get("schwierigkeit")));
+      }
+      csv.close();
+      schreibeInDatei("tmp.xml");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    XMLManager schreiber = new XMLManager();
+    schreiber.konvertiereCSV("./spiel/fragen/fragen.csv");
+  }
+
 }
--- spiel/XMLManagerTest.java
+++ spiel/XMLManagerTest.java
@@ -7,14 +7,14 @@ import java.net.URL;
 
 import org.junit.Test;
 
-public class XMLLeserTest {
+public class XMLManagerTest {
 
   @Test
   public void testeKonstruktor() {
     URL url = Thread.currentThread().getContextClassLoader().getResource("spiel/fragen/musik/musik01.xml");
     File file = new File(url.getPath());
-    XMLLeser leser = new XMLLeser(file);
-    assertEquals("Musik", leser.fach);
+    XMLManager xml = new XMLManager(file);
+    assertEquals("Musik", xml.gibFach());
   }
 
 }
