c8e5871fa16f9aa430105029ea6ea96f0497be2f
Josef Friedrich <josef@friedrich.rocks>
2020-09-18 13:33:45 +0200

    Löschen von nicht benötigten Imports

--- src/main/java/nuernberg/team/spiel/XMLDatei.java
+++ src/main/java/nuernberg/team/spiel/XMLDatei.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
